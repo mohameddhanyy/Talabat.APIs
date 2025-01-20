605804ae7a61e1b3034d4fe39bada171837639c1
Mohameddhanyy <Mohamed_Hani_1179@fci.helwan.edu.eg>
2025-01-21 00:26:40 +0200

    Add Repository

---
+++ Talabat.Repository/BasketRepository.cs
@@ -0,0 +1,37 @@
+﻿using StackExchange.Redis;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+using Talabat.Core.Entities;
+using Talabat.Core.Repositories.Contract;
+
+namespace Talabat.Repository
+{
+    public class BasketRepository : IBasketRepository
+    {
+        private readonly IDatabase _database;
+        public BasketRepository(IConnectionMultiplexer redis)
+        {
+            _database = redis.GetDatabase();
+        }
+        public async Task<bool> DeleteBasketAsync(string basketId)
+        {
+            return await _database.KeyDeleteAsync(basketId);
+        }
+
+        public async Task<CustomerBasket?> GetBasketAsync(string basketId)
+        {
+            var basket = await _database.StringGetAsync(basketId);
+            return basket.IsNullOrEmpty ? null : JsonSerializer.Deserialize<CustomerBasket>(basket);
+        }
+        public async Task<CustomerBasket?> UpdateBasketAsync(CustomerBasket customerBasket)
+        {
+            var createdOrUpdated = await _database.StringSetAsync(customerBasket.Id, JsonSerializer.Serialize(customerBasket),TimeSpan.FromDays(30));
+            if (createdOrUpdated is false) return null;
+            return await GetBasketAsync(customerBasket.Id);
+        }
+    }
+}
