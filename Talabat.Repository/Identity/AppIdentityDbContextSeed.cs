7ccd35d53cfc078294727c6daffddd51b800ea55
Mohameddhanyy <Mohamed_Hani_1179@fci.helwan.edu.eg>
2025-01-22 06:55:12 +0200

    Security module

---
+++ Talabat.Repository/Identity/AppIdentityDbContextSeed.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Talabat.Core.Entities.Identity;
+
+namespace Talabat.Repository.Identity
+{
+    public static class AppIdentityDbContextSeed
+    {
+        public static async Task SeedUserAsync(UserManager<AppUser> _userManger)
+        {
+            if (_userManger.Users.Count()==0)
+            {
+                var user = new AppUser()
+                {
+                    DisplayName = "Mohamed Hany",
+                    Email = "mohany92754@gmail.com",
+                    UserName= "Mohamed.Hany",
+                    PhoneNumber = "01090208738"
+                };
+
+               await _userManger.CreateAsync(user,"Pa@@w0rd");
+
+            }
+        }
+    }
+}
