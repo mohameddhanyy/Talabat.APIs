02bd4ad008ab6cd8239359559d055f6d07723794
Mohameddhanyy <Mohamed_Hani_1179@fci.helwan.edu.eg>
2025-02-14 00:53:27 +0200

    Add Payment Service (Stripe getaway), CreatePaymentIntent

--- Talabat.Core/Entities/CustomerBasket.cs
+++ Talabat.Core/Entities/CustomerBasket.cs
@@ -15,6 +15,12 @@ namespace Talabat.Core.Entities
         }
 
         public string Id { get; set; }
+        public int? DeliveryMethodId { get; set; }
+        public decimal ShippingPrice { get; set; }
+
+
+        public string? PaymentIntentId { get; set; }
+        public string? ClientSecret { get; set; }
         public List<BasketItem> Items { get; set; }
     }
 }
---
+++ Talabat.Service/PaymentService.cs
@@ -0,0 +1,87 @@
+﻿using Microsoft.Extensions.Configuration;
+using Stripe;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Talabat.Core;
+using Talabat.Core.Entities;
+using Talabat.Core.Entities.Order_Aggregate;
+using Talabat.Core.Repositories.Contract;
+using Talabat.Core.Services.Contract;
+using Product = Talabat.Core.Entities.Product;
+
+namespace Talabat.Service
+{
+    public class PaymentService : IPaymentService
+    {
+        private readonly IConfiguration _configuration;
+        private readonly IUnitOfWork _unitOfWork;
+        private readonly IBasketRepository _basketRepository;
+
+        public PaymentService(IConfiguration configuration,
+            IUnitOfWork unitOfWork,
+            IBasketRepository basketRepository)
+        {
+            _configuration = configuration;
+            _unitOfWork = unitOfWork;
+            _basketRepository = basketRepository;
+        }
+        public async Task<CustomerBasket> CreateOrUpdatePaymentIntent(string basketId)
+        {
+            StripeConfiguration.ApiKey = _configuration["StripeSettings:Secretkey"];
+
+            var basket = await _basketRepository.GetBasketAsync(basketId);
+
+            if (basket is null) return null;
+
+            decimal shippingCost = 0m;
+            if(basket.DeliveryMethodId.HasValue)
+            {
+                var deliveryMethod = await _unitOfWork.Repository<DeliveryMethod>().GetAsync(basket.DeliveryMethodId.Value);
+                basket.ShippingPrice = deliveryMethod.Cost;
+                shippingCost = deliveryMethod.Cost;
+            }
+
+            if (basket?.Items.Count != 0)
+            {
+                foreach (var item in basket.Items)
+                {
+                    var product = await _unitOfWork.Repository<Product>().GetAsync(item.Id);
+                    if (item.Price != product.Price)
+                        item.Price = product.Price;
+                }
+            }
+
+            PaymentIntentService paymentIntentService = new PaymentIntentService();
+            PaymentIntent paymentIntent;
+            if (string.IsNullOrEmpty(basket.PaymentIntentId))
+            {
+                var createOptions = new PaymentIntentCreateOptions()
+                {
+                    Amount = (long)basket.Items.Sum(item => item.Price * item.Quantity * 100) + (long)(shippingCost * 100),
+                    Currency = "usd",
+                    PaymentMethodTypes = new List<string>() { "card" }
+                };
+                paymentIntent = await paymentIntentService.CreateAsync(createOptions);
+                basket.PaymentIntentId = paymentIntent.Id;
+            }
+            else
+            {
+                var updateOptions = new PaymentIntentUpdateOptions()
+                {
+                    Amount = (long)basket.Items.Sum(item => item.Price * item.Quantity * 100) + (long)(shippingCost * 100)
+                };
+                paymentIntent = await paymentIntentService.UpdateAsync(basket.PaymentIntentId, updateOptions);
+            }
+
+            await _basketRepository.UpdateBasketAsync(basket);
+            return basket;
+
+
+
+
+        }
+    }
+}
